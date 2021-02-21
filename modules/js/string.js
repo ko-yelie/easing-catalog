bc5743c20833edf1e4f5691a888285ae109e497b
Ko.Yelie <ko.yelie@gmail.com>
2021-02-22 02:21:32 +0900

    divide ease name and type

---
+++ modules/js/string.js
@@ -0,0 +1,75 @@
+/* ----------------------------------------
+  文字列
+---------------------------------------- */
+
+/**
+ * ユニークな文字列を取得
+ * @param {string} [prefix=''] - プレフィックス文字列
+ * @return {string}
+ */
+export function getUniqueId(prefix = '') {
+  window.sdUniqueId = window.sdUniqueId > 0 ? window.sdUniqueId : 0
+  return prefix + String(window.sdUniqueId++)
+}
+
+/**
+ * ゼロパディング
+ * @param {number} number - 数値
+ * @param {number} digits - 桁数
+ * @return {string} ゼロパディングした文字列
+ */
+export function padStartWithZero(number, digits) {
+  return (Array(digits).join('0') + number).slice(-digits)
+}
+
+/**
+ * 2桁のゼロパディング
+ * @param {number} number - 数値
+ * @return {string} ゼロパディングした文字列
+ */
+export function padStartWithZero2(number) {
+  return padStartWithZero(number, 2)
+}
+
+/**
+ * 半角スペースを br タグに変換
+ * @param {string} text - テキスト
+ * @return {string} 変換後のテキスト
+ */
+export function space2br(text) {
+  return text.replace(/ /g, '<br>')
+}
+
+/**
+ * キャメルケースへ変換 sampleString
+ * @param {string}
+ * @return {string}
+ */
+export function camelCase(str) {
+  str = str.charAt(0).toLowerCase() + str.slice(1)
+  return str.replace(/[-_ ](.)/g, function(match, group1) {
+    return group1.toUpperCase()
+  }).trim()
+}
+
+/**
+ * スネークケースへ変換 sample_string
+ * @param {string}
+ * @return {string}
+ */
+export function snakeCase(str) {
+  const camel = camelCase(str)
+  return camel.replace(/[A-Z]/g, function(s) {
+    return '_' + s.charAt(0).toLowerCase()
+  })
+}
+
+/**
+ * パスカルケースへ変換 SampleString
+ * @param {string}
+ * @return {string}
+ */
+export function pascalCase(str) {
+  const camel = camelCase(str)
+  return camel.charAt(0).toUpperCase() + camel.slice(1)
+}
--- pages/rectangle/index.js
+++ pages/rectangle/index.js
@@ -1,51 +1,75 @@
 import { useEffect, useState } from 'react'
 import { className } from '../../modules/js/className'
+import { camelCase } from '../../modules/js/string'
 import styles from './index.module.scss'
 
 const param = {
-  ease: 'easeOutExpo',
+  easeName: 'expo',
+  easeType: 'out',
   duration: 1,
 }
 
 const EASE_LIST = {
-  'ease': 'ease',
-  'ease-in': 'ease-in',
-  'ease-out': 'ease-out',
-  'ease-in-out': 'ease-in-out',
-  'easeInSine': 'cubic-bezier(0.12, 0, 0.39, 0)',
-  'easeOutSine': 'cubic-bezier(0.61, 1, 0.88, 1)',
-  'easeInOutSine': 'cubic-bezier(0.37, 0, 0.63, 1)',
-  'easeInQuad': 'cubic-bezier(0.11, 0, 0.5, 0)',
-  'easeOutQuad': 'cubic-bezier(0.5, 1, 0.89, 1)',
-  'easeInOutQuad': 'cubic-bezier(0.45, 0, 0.55, 1)',
-  'easeInCubic': 'cubic-bezier(0.32, 0, 0.67, 0)',
-  'easeOutCubic': 'cubic-bezier(0.33, 1, 0.68, 1)',
-  'easeInOutCubic': 'cubic-bezier(0.65, 0, 0.35, 1)',
-  'easeInQuart': 'cubic-bezier(0.5, 0, 0.75, 0)',
-  'easeOutQuart': 'cubic-bezier(0.25, 1, 0.5, 1)',
-  'easeInOutQuart': 'cubic-bezier(0.76, 0, 0.24, 1)',
-  'easeInQuint': 'cubic-bezier(0.64, 0, 0.78, 0)',
-  'easeOutQuint': 'cubic-bezier(0.22, 1, 0.36, 1)',
-  'easeInOutQuint': 'cubic-bezier(0.83, 0, 0.17, 1)',
-  'easeInExpo': 'cubic-bezier(0.7, 0, 0.84, 0)',
-  'easeOutExpo': 'cubic-bezier(0.16, 1, 0.3, 1)',
-  'easeInOutExpo': 'cubic-bezier(0.87, 0, 0.13, 1)',
-  'easeInCirc': 'cubic-bezier(0.55, 0, 1, 0.45)',
-  'easeOutCirc': 'cubic-bezier(0, 0.55, 0.45, 1)',
-  'easeInOutCirc': 'cubic-bezier(0.85, 0, 0.15, 1)',
-  'easeInBack': 'cubic-bezier(0.36, 0, 0.66, -0.56)',
-  'easeOutBack': 'cubic-bezier(0.34, 1.56, 0.64, 1)',
-  'easeInOutBack': 'cubic-bezier(0.68, -0.6, 0.32, 1.6)',
-  // 'easeInElastic': 'no',
-  // 'easeOutElastic': 'no',
-  // 'easeInOutElastic': 'no',
-  // 'easeInBounce': 'no',
-  // 'easeOutBounce': 'no',
-  // 'easeInOutBounce': 'no',
+  ease: 'ease',
+  easeIn: 'ease-in',
+  easeOut: 'ease-out',
+  easeInOut: 'ease-in-out',
+  easeInSine: 'cubic-bezier(0.12, 0, 0.39, 0)',
+  easeOutSine: 'cubic-bezier(0.61, 1, 0.88, 1)',
+  easeInOutSine: 'cubic-bezier(0.37, 0, 0.63, 1)',
+  easeInQuad: 'cubic-bezier(0.11, 0, 0.5, 0)',
+  easeOutQuad: 'cubic-bezier(0.5, 1, 0.89, 1)',
+  easeInOutQuad: 'cubic-bezier(0.45, 0, 0.55, 1)',
+  easeInCubic: 'cubic-bezier(0.32, 0, 0.67, 0)',
+  easeOutCubic: 'cubic-bezier(0.33, 1, 0.68, 1)',
+  easeInOutCubic: 'cubic-bezier(0.65, 0, 0.35, 1)',
+  easeInQuart: 'cubic-bezier(0.5, 0, 0.75, 0)',
+  easeOutQuart: 'cubic-bezier(0.25, 1, 0.5, 1)',
+  easeInOutQuart: 'cubic-bezier(0.76, 0, 0.24, 1)',
+  easeInQuint: 'cubic-bezier(0.64, 0, 0.78, 0)',
+  easeOutQuint: 'cubic-bezier(0.22, 1, 0.36, 1)',
+  easeInOutQuint: 'cubic-bezier(0.83, 0, 0.17, 1)',
+  easeInExpo: 'cubic-bezier(0.7, 0, 0.84, 0)',
+  easeOutExpo: 'cubic-bezier(0.16, 1, 0.3, 1)',
+  easeInOutExpo: 'cubic-bezier(0.87, 0, 0.13, 1)',
+  easeInCirc: 'cubic-bezier(0.55, 0, 1, 0.45)',
+  easeOutCirc: 'cubic-bezier(0, 0.55, 0.45, 1)',
+  easeInOutCirc: 'cubic-bezier(0.85, 0, 0.15, 1)',
+  easeInBack: 'cubic-bezier(0.36, 0, 0.66, -0.56)',
+  easeOutBack: 'cubic-bezier(0.34, 1.56, 0.64, 1)',
+  easeInOutBack: 'cubic-bezier(0.68, -0.6, 0.32, 1.6)',
+  // easeInElastic: 'no',
+  // easeOutElastic: 'no',
+  // easeInOutElastic: 'no',
+  // easeInBounce: 'no',
+  // easeOutBounce: 'no',
+  // easeInOutBounce: 'no',
 }
 
+const EASE_NAME_LIST = [
+  'ease',
+  'sine',
+  'quad',
+  'cubic',
+  'quart',
+  'quint',
+  'expo',
+  'circ',
+  'back',
+  // 'elastic',
+  // 'bounce',
+]
+
+const EASE_TYPE_LIST = [
+  '',
+  'in',
+  'out',
+  'inOut',
+]
+
 export default function Rectangle() {
-  const [ease, setEase] = useState(param.ease)
+  const [easeName, setEaseName] = useState(param.easeName)
+  const [easeType, setEaseType] = useState(param.easeType)
   const [duration, setDuration] = useState(param.duration)
   const [isShow, setIsShow] = useState(false)
 
@@ -64,8 +88,14 @@ export default function Rectangle() {
   const handleChange = (event) => {
     const { name, value } = event.currentTarget
     switch (name) {
-      case 'ease':
-        setEase(value)
+      case 'easeName':
+        setEaseName(value)
+        if (value !== 'ease' && easeType === '') {
+          setEaseType('in')
+        }
+        break
+      case 'easeType':
+        setEaseType(value)
         break
       case 'duration':
         if (value === '') return
@@ -75,7 +105,7 @@ export default function Rectangle() {
     play()
   }
 
-  const easeValue = EASE_LIST[ease]
+  const ease = EASE_LIST[camelCase(`ease ${easeType} ${easeName === 'ease' ? '' : easeName}`)]
 
   return (
     <>
@@ -85,7 +115,7 @@ export default function Rectangle() {
           style={
             isShow
               ? {
-                  transitionTimingFunction: easeValue,
+                  transitionTimingFunction: ease,
                   transitionDuration: `${duration}s`,
                 }
               : null
@@ -93,29 +123,49 @@ export default function Rectangle() {
         ></div>
       </div>
 
-      <div>
-        ease:{' '}
-        <select name="ease" value={ease} onChange={handleChange}>
-          {Object.keys(EASE_LIST).map((key) => (
-            <option value={key} key={key}>{key}</option>
-          ))}
-        </select>
-        {' '}
-        <small>{`(${easeValue})`}</small>
-      </div>
+      <dl>
+        <dt>ease:</dt>
+        <dd>
+          <dl>
+            <dt>name:</dt>
+            <dd>
+              <select name="easeName" value={easeName} onChange={handleChange}>
+                {EASE_NAME_LIST.map((value) => (
+                  <option value={value} key={value}>{value}</option>
+                ))}
+              </select>
+            </dd>
 
-      <div>
-        duration:{' '}
-        <input
-          name="duration"
-          type="number"
-          step="0.1"
-          value={duration}
-          onChange={handleChange}
-        ></input>s
-        {' '}
-        <small>{`(${duration * 1000}ms)`}</small>
-      </div>
+            <dt>type:</dt>
+            <dd>
+              <select name="easeType" value={easeType} onChange={handleChange}>
+                {EASE_TYPE_LIST.map((value) =>
+                  easeName !== 'ease' && value === ''
+                    ? null
+                    : (
+                      <option value={value} key={value}>{value}</option>
+                    )
+                )}
+              </select>
+            </dd>
+          </dl>
+
+          <small>{`(${ease})`}</small>
+        </dd>
+
+        <dt>duration:</dt>
+        <dd>
+          <input
+            name="duration"
+            type="number"
+            step="0.1"
+            value={duration}
+            onChange={handleChange}
+          ></input>s
+          {' '}
+          <small>{`(${duration * 1000}ms)`}</small>
+        </dd>
+      </dl>
 
       <div>
         <button onClick={play}>Play</button>
