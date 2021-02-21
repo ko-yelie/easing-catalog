e36bcd4863add77ce4e4a952ee109f0a21247aab
Ko.Yelie <ko.yelie@gmail.com>
2021-02-22 01:30:29 +0900

    add basic UI

--- pages/rectangle/index.js
+++ pages/rectangle/index.js
@@ -1,22 +1,125 @@
 import { useEffect, useState } from 'react'
-import { className } from '../../utils/className'
+import { className } from '../../modules/js/className'
 import styles from './index.module.scss'
 
+const param = {
+  ease: 'easeOutExpo',
+  duration: 1,
+}
+
+const EASE_LIST = {
+  'ease': 'ease',
+  'ease-in': 'ease-in',
+  'ease-out': 'ease-out',
+  'ease-in-out': 'ease-in-out',
+  'easeInSine': 'cubic-bezier(0.12, 0, 0.39, 0)',
+  'easeOutSine': 'cubic-bezier(0.61, 1, 0.88, 1)',
+  'easeInOutSine': 'cubic-bezier(0.37, 0, 0.63, 1)',
+  'easeInQuad': 'cubic-bezier(0.11, 0, 0.5, 0)',
+  'easeOutQuad': 'cubic-bezier(0.5, 1, 0.89, 1)',
+  'easeInOutQuad': 'cubic-bezier(0.45, 0, 0.55, 1)',
+  'easeInCubic': 'cubic-bezier(0.32, 0, 0.67, 0)',
+  'easeOutCubic': 'cubic-bezier(0.33, 1, 0.68, 1)',
+  'easeInOutCubic': 'cubic-bezier(0.65, 0, 0.35, 1)',
+  'easeInQuart': 'cubic-bezier(0.5, 0, 0.75, 0)',
+  'easeOutQuart': 'cubic-bezier(0.25, 1, 0.5, 1)',
+  'easeInOutQuart': 'cubic-bezier(0.76, 0, 0.24, 1)',
+  'easeInQuint': 'cubic-bezier(0.64, 0, 0.78, 0)',
+  'easeOutQuint': 'cubic-bezier(0.22, 1, 0.36, 1)',
+  'easeInOutQuint': 'cubic-bezier(0.83, 0, 0.17, 1)',
+  'easeInExpo': 'cubic-bezier(0.7, 0, 0.84, 0)',
+  'easeOutExpo': 'cubic-bezier(0.16, 1, 0.3, 1)',
+  'easeInOutExpo': 'cubic-bezier(0.87, 0, 0.13, 1)',
+  'easeInCirc': 'cubic-bezier(0.55, 0, 1, 0.45)',
+  'easeOutCirc': 'cubic-bezier(0, 0.55, 0.45, 1)',
+  'easeInOutCirc': 'cubic-bezier(0.85, 0, 0.15, 1)',
+  'easeInBack': 'cubic-bezier(0.36, 0, 0.66, -0.56)',
+  'easeOutBack': 'cubic-bezier(0.34, 1.56, 0.64, 1)',
+  'easeInOutBack': 'cubic-bezier(0.68, -0.6, 0.32, 1.6)',
+  // 'easeInElastic': 'no',
+  // 'easeOutElastic': 'no',
+  // 'easeInOutElastic': 'no',
+  // 'easeInBounce': 'no',
+  // 'easeOutBounce': 'no',
+  // 'easeInOutBounce': 'no',
+}
+
 export default function Rectangle() {
-  const [show, setShow] = useState(false)
+  const [ease, setEase] = useState(param.ease)
+  const [duration, setDuration] = useState(param.duration)
+  const [isShow, setIsShow] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => {
-      setShow(true)
-    }, 1000)
-  })
+    if (!isShow) {
+      requestAnimationFrame(() => {
+        setIsShow(true)
+      })
+    }
+  }, [isShow])
+
+  const play = () => {
+    setIsShow(false)
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.currentTarget
+    switch (name) {
+      case 'ease':
+        setEase(value)
+        break
+      case 'duration':
+        if (value === '') return
+        setDuration(Number(value))
+        break
+    }
+    play()
+  }
+
+  const easeValue = EASE_LIST[ease]
 
   return (
     <>
-      <div className={className(styles.rectangle, {[styles._show]: show})}>
+      <div className={className(styles.rectangle, {[styles._show]: isShow})}>
+        <div
+          className={styles.inner}
+          style={
+            isShow
+              ? {
+                  transitionTimingFunction: easeValue,
+                  transitionDuration: `${duration}s`,
+                }
+              : null
+          }
+        ></div>
       </div>
-      <style jsx>{`
-      `}</style>
+
+      <div>
+        ease:{' '}
+        <select name="ease" value={ease} onChange={handleChange}>
+          {Object.keys(EASE_LIST).map((key) => (
+            <option value={key} key={key}>{key}</option>
+          ))}
+        </select>
+        {' '}
+        <small>{`(${easeValue})`}</small>
+      </div>
+
+      <div>
+        duration:{' '}
+        <input
+          name="duration"
+          type="number"
+          step="0.1"
+          value={duration}
+          onChange={handleChange}
+        ></input>s
+        {' '}
+        <small>{`(${duration * 1000}ms)`}</small>
+      </div>
+
+      <div>
+        <button onClick={play}>Play</button>
+      </div>
     </>
   )
 }
